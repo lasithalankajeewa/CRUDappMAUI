d18f478a04f628df62f1d5da2e746ef5d37109f4
Lasitha Lankajeewa <lasithalankajeewa@gmail.com>
2022-12-09 10:59:19 +0530

    Leave request page final version

--- CRUDappMAUI/MainPage.xaml.cs
+++ CRUDappMAUI/MainPage.xaml.cs
@@ -5,19 +5,27 @@ namespace CRUDappMAUI;
 
 public partial class MainPage : ContentPage
 {
-	
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
 
     async void OnAddLeaveClicked(object sender, EventArgs e)
     {
         Debug.WriteLine("--Add Leave button Clicked");
-        
+
 
         await Shell.Current.GoToAsync(nameof(AddLeavePage));
     }
+
+    async void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        Debug.WriteLine("--Item Changed clicked");
+
+
+    }
+
 }
 
